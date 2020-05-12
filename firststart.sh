88ddbad24752e18a8c896cf9ce4252a4848910fd
student <student@life-edu.eu>
2020-05-12 09:51:21 +0200

    fixed dontagain - fixed do not start in livemode

--- firststart.sh
+++ firststart.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-# last updated: 17.11.2018
+# last updated: 12.05.2020
 #delete yourself
 
 USER=$(logname)   #logname seems to always deliver the current xsession user - no matter if you are using SUDO
 HOME="/home/${USER}/"
 EXAMLOCKFILE="${HOME}.life/EXAM/exam.lock"
 
-COW=$(blkid | grep casper | wc -l)
+#COW=$(blkid | grep casper | wc -l)
+NOPERSISTENT=$(cat /proc/cmdline |grep nopersistent |wc -l)
 
 
-if test $COW = "0" 
+
+#test for the kernel paramter "nopersistent" (live mode)
+if test $NOPERSISTENT = "1" 
 then
     echo "life mode - exit"
     exit 0
@@ -25,7 +28,7 @@ fi
 
 
 
-kdialog --dontagain firststart:nofirststart --title "First Start Wizard"  --yesno "Möchten Sie das System jetzt einrichten?"
+kdialog --dontagain firststart:'Nicht mehr nachfragen' --title "First Start Wizard"  --yesno "Möchten Sie das System jetzt einrichten?"
 
 if [ "$?" = 0 ]; then
     rm /home/student/.config/autostart-scripts/firststart.sh
